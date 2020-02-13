1931bbd880755b1bedb43d512a3ac6bf8ddec475
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-12 18:24:13 -1000

    Characters rendering test implemented

--- star-wars-app/src/App.test.js
+++ star-wars-app/src/App.test.js
@@ -13,3 +13,4 @@ test("Render App header", () => {
 
     expect(element).toBeVisible();
 })
+
--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -3,3 +3,10 @@ import * as rtl from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import StarWarsCharacters from './StarWarsCharacters'
 import axios from 'axios';
+
+
+test("Render of star wars characters", async () => {
+    const wrapper = rtl.render(<StarWarsCharacters />);
+    const character = await wrapper.findAllByTestId("character");
+    expect(character[0]).toBeVisible();
+})
